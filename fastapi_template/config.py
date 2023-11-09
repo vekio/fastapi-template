7da8ccc406d45dbb5f7bfc692016f8c6b4529e99
Alberto Castañeiras <alberto@casta.me>
2023-11-09 20:47:06 +0100

    change startup

--- fastapi_template/config.py
+++ fastapi_template/config.py
@@ -5,13 +5,6 @@ class Settings(BaseSettings):
     project: str
     version: str
     api_version: str
-    api_host: str
-    api_port: int
-    # web_host: str
-    # web_port: int
-
-    # model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
-print(settings)
